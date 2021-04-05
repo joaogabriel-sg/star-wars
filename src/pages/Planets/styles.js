cb490c3363ed6bc369bef8de626433f961c70d3b
João Gabriel <joaogabrielsg.00@gmail.com>
2021-04-05 14:02:41 -0300

    :iphone: add responsive mobile style to the planets page

--- src/pages/Planets/styles.js
+++ src/pages/Planets/styles.js
@@ -4,4 +4,9 @@ export const Container = styled.section`
   width: 100%;
   padding: 4rem 8rem;
   overflow-y: auto;
+
+  @media screen and (max-width: 820px) {
+    padding: 2rem 4rem;
+    overflow-y: visible;
+  }
 `;
